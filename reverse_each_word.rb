08016fc37f90d7c60e4f4842a6bdc722ccb768b8
Blakeley Bond <burrick2003@yahoo.com>
2017-10-31 03:25:03 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(input)
   array = []
   array = input.split(" ")
-  array.each_with_index do |element, index|
+  array.map_with_index do |element, index|
     array[index] = array[index].reverse
   end
   output = array.join(" ")
